28fd1a23458014b82e2e24701f9e1ab98270f8cc
bucu0368 <liembuib123@gmail.com>
2025-09-04 03:42:05 +0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
-ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+NAME = "ThunderX"
+server = "https://discord.gg/Zg2XkS5hq9"
+ch = "https://discord.com/channels/1385273333720940778/1393807816937963636"
+OWNER_IDS = [1142053791781355561, 1142053791781355561]
+BotName = "ThunderX"
+serverLink = "https://discord.gg/Zg2XkS5hq9"
